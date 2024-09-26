93278c9f4b05d72ffedc9e5b7f15597d458aeacb
akarshhkh <akarshkhot@gmail.com>
2024-09-26 12:14:57 +0530

    Create positive_or_negative.cpp

---
+++ positive_or_negative.cpp
@@ -0,0 +1,30 @@
+//Akarsh khot
+//23070123012
+//Aim - to verify whether the give number by the user is positive or negative.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    if (a>=0)
+    {
+        cout<<"It is a positive number.";
+    }
+    else
+    {
+        cout<<"It is a negative number.";
+    }
+}
+
+/*
+OUTPUT:
+Enter a number: 5
+It is a positive number.
+
+Enter a number: -6
+It is a negative number.
+*/
